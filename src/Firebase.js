1cb50b6499623b03bb15feba7c6b07ae08cc3a52
peanutyabing <sherry.liuyabing@gmail.com>
2023-03-09 11:05:31 +0800

    Set up Firebase config

---
+++ src/Firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_REALTIME_DATABASE_URL,
+  projectId: process.env.REACT_APP_REALTIME_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MSG_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+export const database = getDatabase(firebaseApp);
+export const storage = getStorage(firebaseApp);
+export const auth = getAuth(firebaseApp);
