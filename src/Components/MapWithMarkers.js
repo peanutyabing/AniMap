5c82da603144e1b3610d0de51d668e608e338f9b
peanutyabing <sherry.liuyabing@gmail.com>
2023-03-09 14:31:09 +0800

    Refactor Marker to be a standalone component and child of Map

--- src/Components/MapWithMarkers.js
+++ src/Components/MapWithMarkers.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
 import catIcon from "../Icons/cat-50.png";
 import otterIcon from "../Icons/otter-60.png";
+import { AnimalMarker } from "./Marker";
 
 const containerStyle = {
   width: "100vw",
@@ -24,16 +25,23 @@ const positions = [
   },
 ];
 
-const onLoad = (marker) => {
-  console.log("marker: ", marker);
+const animals = {
+  cat: catIcon,
+  otter: otterIcon,
 };
+
+// const onLoad = (marker) => {
+//   console.log("marker: ", marker);
+// };
 
 export default function MapWithMarkers() {
   return (
     <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
       <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={12}>
-        <Marker onLoad={onLoad} position={positions[0]} icon={catIcon} />
-        <Marker onLoad={onLoad} position={positions[1]} icon={otterIcon} />
+        <AnimalMarker position={positions[0]} animal={animals.cat} />
+        <AnimalMarker position={positions[1]} animal={animals.otter} />
+        {/* <Marker onLoad={onLoad} position={positions[0]} icon={catIcon} />
+        <Marker onLoad={onLoad} position={positions[1]} icon={otterIcon} /> */}
       </GoogleMap>
     </LoadScript>
   );
---
+++ src/Components/Marker.js
@@ -0,0 +1,10 @@
+import { Marker } from "@react-google-maps/api";
+
+export function AnimalMarker(props) {
+  const onLoad = (marker) => {
+    console.log("marker: ", marker);
+  };
+  return (
+    <Marker onLoad={onLoad} position={props.position} icon={props.animal} />
+  );
+}
